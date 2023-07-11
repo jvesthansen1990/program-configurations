5712ca0ba3f0bcac1101f7e43b913b75642e2aa1
jvesthansen1990 <42712599+jvesthansen1990@users.noreply.github.com>
2023-07-11 20:30:06 +0200

    added comments and more commands for PS Profile

--- windows-terminal/configurations-file/Microsoft.PowerShell_profile.ps1
+++ windows-terminal/configurations-file/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,18 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-Prompt
-Set-Theme Paradox
+
+# makes very nice ctrl+r search menu
+Import-Module psfzf
+
 Remove-PSReadlineKeyHandler 'Ctrl+r'
 Remove-PSReadlineKeyHandler 'Ctrl+t'
-Import-Module psfzf
+
+# Set Posh theme
+Set-Theme Paradox
+
+# Gives command suggestions based on history
+Set-PSReadLineOption -PredictionSource History
+# Gives a listview when typing commands
+set-psReadLineOption -PredictionViewStyle ListView
+# Set the color of InlinePrediction
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#F50606'}
